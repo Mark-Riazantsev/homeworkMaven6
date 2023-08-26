3fabd0a610520ca9a8f4911bf38b27e9d723f852
Riazantsev <riazantsevmark327@gmail.com>
2023-08-26 11:07:20 +0400

    methods added

---
+++ src/main/java/Radio.java
@@ -0,0 +1,34 @@
+public class Radio {
+ public int currentRadioStation;
+ public  int currentVolume;
+
+    public void increaseRadioStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation++;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+public void decreaseRadioStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation--;
+        } else {
+            currentRadioStation = 9;
+        }
+}
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        } else {
+            currentVolume = 100;
+        }
+        }
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        } else {
+            currentVolume = 0;
+        }
+
+    }
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void shouldSetRadioStation() {
+
+
+    }
+
+    @Test
+    public void
+}
+
